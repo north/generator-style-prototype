e265abb672425bc33e21022ea64949cccef0ce75
Sam Richard <snugug@gmail.com>
2013-06-13 15:38:45 -0400

    New ASCII Art, more robust prompts, updated copy over

--- app/index.js
+++ app/index.js
@@ -3,6 +3,8 @@ var util = require('util');
 var path = require('path');
 var spawn = require('child_process').spawn;
 var yeoman = require('yeoman-generator');
+var _s = require('underscore.string');
+var check = require('validator').check;
 
 
 var AppGenerator = module.exports = function Appgenerator(args, options, config) {
@@ -77,31 +79,66 @@ AppGenerator.prototype.askFor = function askFor() {
   var cb = this.async();
 
   var welcome =
-  '\n             ,.-----__'.red +
-  '\n          ,:::://///,:::-.'.red +
-  '\n        /:\'\'/////// ``:::`;/|/'.red + '     .------------------.'.magenta +
-  '\n       /\'   ||||||     :://\'`\\'.red + '     | '.magenta + 'I\'m an Armadillo'.yellow.bold + ' |'.magenta +
-  '\n      .\' ,   ||||||     `/(  '.red + 'e'.white + ' \\'.red + '   \'------------------\'' .magenta +
-  '\n-===~__-\'\\__X_`````\\_____/~`-._ `.'.red + '  '.magenta +
-  '\n            ~~        ~~       `~-\''.red + '\n';
+  '\n ______     ______   __  __     __         ______'.magenta +
+  '\n/\\  ___\\   /\\__  _\\ /\\ \\_\\ \\   /\\ \\       /\\  ___\\      '.magenta + 'D  B  S  i'.yellow.bold +
+  '\n\\ \\___  \\  \\/_/\\ \\/ \\ \\____ \\  \\ \\ \\____  \\ \\  __\\      '.magenta + 'e  u  i  n'.yellow.bold +
+  '\n \\/\\_____\\    \\ \\_\\  \\/\\_____\\  \\ \\_____\\  \\ \\_____\\    '.magenta + 's  i  g'.yellow.bold +
+  '\n  \\/_____/'.magenta+ '_'.cyan + '   '.magenta+ '_'.cyan + '\\/_/'.magenta+ '_'.cyan + '  \\/_____/   \\/_____/'.magenta+ '_'.cyan + '  \\/_____/    '.magenta + 'i  l  n  B'.yellow.bold +
+  '\n    /_____/\\ /_____/\\  /_____/\\ /________/\\/_____/\\'.cyan + '     g  d     r'.yellow.bold +
+  '\n    \\:::_ \\ \\\\:::_ \\ \\ \\:::_ \\ \\\\__.::.__\\/\\:::_ \\ \\'.cyan + '    n     o  o'.yellow.bold +
+  '\n     \\:(_) \\ \\\\:(_) ) )_\\:\\ \\ \\ \\  \\::\\ \\   \\:\\ \\ \\ \\'.cyan + '         f  w'.yellow.bold +
+  '\n      \\: ___\\/ \\: __ `\\ \\\\:\\ \\ \\ \\  \\::\\ \\   \\:\\ \\ \\ \\'.cyan + '        f  s'.yellow.bold +
+  '\n       \\_\\/_____\\_\\/_\\_\\/_\\_____\\/_  \\__\\/__  \\_____\\/'.cyan + '           e'.yellow.bold +
+  '\n        /________/\\/_/\\/_/\\ /_____/\\ /_____/\\ /_____/\\'.cyan + '           r'.yellow.bold +
+  '\n        \\__.::.__\\/\\ \\ \\ \\ \\\\:::_ \\ \\\\::::_\\/_\\::::_\\/_'.cyan +
+  '\n           \\::\\ \\   \\:\\_\\ \\ \\\\:(_) \\ \\\\:\\/___/\\\\:\\/___/\\'.cyan +
+  '\n            \\::\\ \\   \\::::_\\/ \\: ___\\/ \\::___\\/_\\_::._\\:\\'.cyan +
+  '\n             \\::\\ \\    \\::\\ \\  \\ \\ \\    \\:\\____/\\ /____\\:\\'.cyan +
+  '\n              \\__\\/     \\__\\/   \\_\\/     \\_____\\/ \\_____\\/'.cyan;
 
   console.log(welcome);
   console.log("\nOut of the box I include Sass+Compass, Generator for Handlebars templating and Markdown powered pages, Image Optimization, JavaScript Hinting and Minification, and CSS Linting. I also make publishing sites to GitHub Pages very easy. If you have any questions, ask my handler at https://github.com/Snugug/generator-armadillo.\n");
 
     var prompts = [
       {
-        name: 'projectName',
-        message: 'The name of your project. (Required)',
+        name: 'clientName',
+        message: 'The name of your client. (Required)',
         default: '',
-        warning: 'You did not name your project',
+        warning: 'You did include your client name.',
         required: true
       },
       {
-        name: 'requireJS',
-        message: 'Would you like to include RequireJS?',
-        default: true,
-        warning: 'Yes: RequireJS will be placed into the JavaScript vendor directory.'
+        name: 'clientHomepage',
+        message: 'The client\'s URL. (Required)',
+        default: '',
+        warning: 'You need to include your client URL',
+        required: true,
+        validate: function(value) {
+          console.log(value);
+          return false;
+        }
+      },
+      {
+        name: 'authorName',
+        message: 'The author of the Style Prototype. (Required)',
+        default: '',
+        warning: 'You did not include the author',
+        required: true
+      },
+      {
+        name: 'authorEmail',
+        message: 'The email address of the author. (Required)',
+        default: '',
+        warning: 'You did not include the author email address',
+        required: true
       }
+      //,
+      // {
+      //   name: 'requireJS',
+      //   message: 'Would you like to include RequireJS?',
+      //   default: true,
+      //   warning: 'Yes: RequireJS will be placed into the JavaScript vendor directory.'
+      // }
     ];
 
   if (this.options['git']) {
@@ -129,23 +166,28 @@ AppGenerator.prototype.askFor = function askFor() {
   });
 
   this.prompt(prompts, function (err, props) {
+
     if (err) {
       return this.emit('error', err);
     }
 
     // manually deal with the response, get back and store the results.
     // we change a bit this way of doing to automatically do this in the self.prompt() method.
-    this.projectName = props.projectName;
-    this.projectSlug = this.projectName.toLowerCase().replace(/\s+/g, '-');
+    this.clientName = props.clientName;
+    this.clientSlug = _s.slugify(this.clientName);
+    this.clientCamelCase = this.clientName.replace(/\s+/g, '');
+    this.clientHomepage = props.clientHomepage;
+    this.authorName = props.authorName;
+    this.authorEmail = props.authorEmail;
     this.ghRepo = props.ghRepo;
     // this.devHost = props.devHost;
     // this.devPort = props.devPort;
-    this.requireJS = props.requireJS;
+    // this.requireJS = props.requireJS;
     this.ghDeploy = props.ghDeploy;
 
     this.projectDir = './';
     if (this.options['new-dir']) {
-      this.projectDir = this.projectSlug + '/';
+      this.projectDir = this.clientSlug + '/';
     }
 
 
@@ -194,12 +236,22 @@ AppGenerator.prototype.app = function app() {
   this.copy('helpers.js', this.projectDir + 'helpers.js');
   this.copy('Gemfile', this.projectDir + 'Gemfile');
   this.copy('system.json', this.projectDir + '.system.json');
-  this.template('_config.json', this.projectDir + 'config.json');
+  this.template('_config.yml', this.projectDir + 'config.yml');
 
   this.mkdir(this.projectDir + 'images');
   this.directory('sass', this.projectDir + 'sass');
+  this.mkdir(this.projectDir + 'sass/partials');
   this.directory('pages', this.projectDir + 'pages');
 
   this.template('index.html', this.projectDir + 'templates/index.html');
   this.template('main.js', this.projectDir + 'js/main.js');
+};
+
+AppGenerator.prototype.compass = function compass() {
+  var today = new Date();
+  this.today = today.getFullYear() + '-' + ('0' + (today.getMonth()+1)).slice(-2) + '-' + ('0' + (today.getDate())).slice(-2);
+
+  this.copy('extension.json', this.projectDir + '.extension.json');
+  this.template('styleguide.gemspec', this.projectDir + '.compass/' + this.clientSlug + '-styleguide.gemspec');
+  this.copy('styleguide.rb', this.projectDir + '.compass/.template/' + this.clientSlug + '-styleguide.rb');
 };
