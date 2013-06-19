7f1ddb5da3f7561e222f3635558152c2c0c7c9cd
Sam Richard <snugug@gmail.com>
2013-06-19 16:57:04 -0400

    Lots of new helpers

--- app/templates/helpers.js
+++ app/templates/helpers.js
@@ -1,3 +1,13 @@
+var marked = require('marked');
+marked.setOptions({
+  gfm: true,
+  langPrefix: 'language-',
+  tables: true,
+  breaks: true,
+  smartLists: true,
+  smartypants: true
+});
+
 module.exports = {
   'image-url': function(src) {
     return this.options.assets + '/' + this.options.userConfig.assets.imagesDir + '/' + src;
@@ -37,10 +47,42 @@ module.exports = {
   },
   'page-title': function() {
     if (this.page.title) {
-      return this.page.title + ' | ' + this.options.userConfig.project;
+      return this.page.title + ' | ' + this.options.userConfig.client.name + ' Style Prototype';
     }
     else {
-      return this.options.userConfig.project;
+      return this.options.userConfig.client.name + ' Style Prototype';
     }
+  },
+  'classify': function(list) {
+    return list.toString().replace(/,/g , ' ');
+  },
+  'show-example': function() {
+    return this.page.examples;
+  },
+  'markdown': function(md) {
+    return marked(md.fn(1));
+  },
+  'for': function(from, to, incr, block) {
+    var accum = '';
+    for (var i = from; i < to; i += incr) {
+      accum += block.fn(i);
+    }
+    return accum;
+  },
+  'possible-pattern': function(index, src, context) {
+    var output = "<li class='pattern-" + index + "'><img src='";
+
+    var ext = src.match('^(http|https):\/\/');
+
+    if (ext) {
+      output += src;
+    }
+    else {
+      output += context.options.assets + '/' + context.options.userConfig.assets.imagesDir + '/' + src;
+    }
+
+    output += "' alt='Patten " + index + "'></li>";
+
+    return output;
   }
 };
