47d5a7431e77390a81e5d372d5e54fdef78f29ef
Sam Richard <snugug@gmail.com>
2013-09-11 18:04:38 -0400

    Removed mention of Bundler

--- app/index.js
+++ app/index.js
@@ -63,7 +63,7 @@ var AppGenerator = module.exports = function Appgenerator(args, options, config)
       });
 
       // Log the install
-      console.log("\nI'm all done! If your installs did not finish properly, run ".white + "bundle install & bower install & npm install".yellow + " to finish installation.");
+      console.log("\nI'm all done! If your installs did not finish properly, run ".white + "bower install & npm install".yellow + " to finish installation.");
     }
     else {
       var bye = "\n I'm all done! Now run ".white + "bower install & npm install".yellow + " to finish installation.";
